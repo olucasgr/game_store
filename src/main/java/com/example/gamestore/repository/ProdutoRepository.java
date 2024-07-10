218e269064a850d54f73cd9919e1373625df53da
Lucasgr <lucasrctt@gmail.com>
2024-07-10 13:25:16 -0300

    inclusão da camada repository

---
+++ src/main/java/com/example/gamestore/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.example.gamestore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.gamestore.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
